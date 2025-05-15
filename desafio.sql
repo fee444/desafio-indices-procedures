ce301163e3267b5a5246a89a5cba155ed7afecbe
fee444 <166446087+fee444@users.noreply.github.com>
2025-05-15 17:00:41 -0300

    Desafio concluído!

---
+++ desafio.sql
@@ -0,0 +1,50 @@
+-- CRIANDO INDICES 
+use company;
+CREATE INDEX idx_full_name ON employee(Fname,Lname);
+CREATE INDEX idx_name_project ON project(Pname);
+
+SELECT 
+    TABLE_NAME, 
+    COLUMN_NAME, 
+    CONSTRAINT_NAME, 
+    REFERENCED_TABLE_NAME, 
+    REFERENCED_COLUMN_NAME
+FROM 
+    information_schema.KEY_COLUMN_USAGE
+WHERE 
+    TABLE_SCHEMA = 'company'
+    AND REFERENCED_TABLE_NAME IS NOT NULL;
+
+-- Qual departamento com o maior número de pessoas?
+SELECT COUNT(e.Ssn) qntd_pessoas, d.Dnumber
+FROM departament d
+INNER JOIN employee e 
+ON d.Dnumber = e.Dno
+GROUP BY d.Dnumber;
+
+-- Quais são os departamentos por cidade?
+SELECT * FROM dept_locations;
+
+-- Relação de empregados por departamento
+SELECT CONCAT(e.Fname,' ', e.Minit, ' ', e.Lname) nome_empregado, d.Dname nome_departamento
+	FROM employee e 
+    INNER JOIN departament d
+    ON e.Dno = d.Dnumber;
+    
+    -- Parte 02 Desafio
+    DROP PROCEDURE manage_emplpoyee;
+    DELIMITER //
+    CREATE PROCEDURE manage_emplpoyee(IN options INT)
+		BEGIN
+		CASE 
+		WHEN options = 1 THEN SELECT CONCAT(Fname, ' ', Minit, ' ', LName) nome_completo FROM employee;
+		WHEN options = 2 THEN UPDATE employee SET Fname = 'Neymar' WHERE Ssn = '33000.00';
+		WHEN options = 3 THEN DELETE FROM employee WHERE Ssn = '33000.00';
+		ELSE
+			SELECT 'Escolha uma opção válida! (1,2 ou 3)' erro;
+		END CASE;
+	END
+    //
+    DELIMITER ;
+
+
